371be4865269086be2b319a5ff57901307bb532a
RetracedFn <retracedfn@gmail.com>
2024-07-12 17:28:37 +0000

    Update testing self code ext.cpp

--- testing self code ext/testing self code ext.cpp
+++ testing self code ext/testing self code ext.cpp
@@ -1,13 +1,9 @@
-// testing self code ext.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "driver.h"
 int main()
 {
-	//SetConsoleTitleA("Payson IOCTL - github.com/paysonism - Usermode Example");
+	
 	if (!mem::find_driver()) {
-		system("color 2");
 		std::cout << "\n Driver isn't loaded!\n";
 	}
 	mem::process_id = mem::find_process(L"FortniteClient-Win64-Shipping.exe");
