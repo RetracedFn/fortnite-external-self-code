05b3397021cd4352571f93d4138a0769b9a698ab
RetracedFn <retracedfn@gmail.com>
2024-07-12 10:30:43 +0000

    offsets and set up

---
+++ testing self code ext/offsets.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "main.cpp"
+
+
+namespace Addresses
+{
+	uintptr_t GWorld = 0x117105C8; 
+	uintptr_t GObjects = 0x12954990;
+	uintptr_t GNames = 0x12BA9140;
+	uintptr_t FName = 0x117284C0; 
+	uintptr_t GetBoneMatrix = 0x1EB8938;
+	uintptr_t LineOfSightTo = 0x627FB84;
+	uintptr_t Viewpoint = 0x10F90A10;
+	uintptr_t DrawTransition = 0x71;
+	uintptr_t ProcessEvent = 0x4D;
+}
